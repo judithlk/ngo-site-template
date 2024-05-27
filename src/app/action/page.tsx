76b66027b57db1e2eb4d3403937a77e3368a637d
Judith <104207025+judithlk@users.noreply.github.com>
2024-05-27 11:58:01 +0100

    Volunteer modal

---
+++ src/app/action/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import SocialBar from "../components/SocialBar";
+import Image from "next/image";
+import Link from "next/link";
+import VolunteerModal from "../components/VolunteerModal";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
+export default function TakeAction() {
+  const [modalState, setModalState] = useState(false);
+  const showModal = () => {
+    setModalState(true);
+  };
+
+  return (
+    <>
+      <NavBar />
+      <main className="w-[100%] pb-10 mb-0 relative">
+        <div className="w-[100%] h-[100%] pt-[10%] lg:pt-0 space-y-10">
+          <div className="h-64 md:h-80 bg-darkblue flex items-center justify-center pt-4">
+            <h1 className="text-3xl md:text-5xl text-white font-bold italic">
+              What can you do?
+            </h1>
+          </div>
+          <div className="w-[100%] py-4 px-10 space-y-8">
+            <h1 className="text-4xl md:text-5xl font-semibold text-midblue">
+              VOLUNTEER
+            </h1>
+            <div className="flex flex-col sm:flex-row justify-around lg:justify-normal">
+              <div className="sm:w-[70%] flex flex-col justify-center">
+                <p className="italic">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptate aut beatae temporibus quia fugiat culpa
+                  necessitatibus doloribus rerum corrupti labore fugit nesciunt
+                  ut, aliquid repellat ipsum perspiciatis illum quaerat
+                  distinctio. Lorem ipsum dolor sit amet, consectetur
+                  adipisicing elit. Debitis dolores nostrum eos architecto? Fuga
+                  voluptate libero fugit minima perferendis ducimus tempora
+                  distinctio possimus, dolorum corporis, dicta consectetur
+                  nesciunt omnis maiores.
+                </p>
+                <h2 className="text-midblue">
+                  -
+                  <Link href="#" className="hover:text-darkblue">
+                    Volunteer name
+                  </Link>
+                  , 23
+                </h2>
+              </div>
+              <div className="relative w-[52%] mt-5 sm:mt-0 sm:w-[25%] lg:w-[18%] md:ml-10 rounded-full">
+                <Image
+                  src={"/profile photo.jpg"}
+                  alt=""
+                  layout="responsive"
+                  width={100}
+                  height={100}
+                  style={{ borderRadius: "50%" }}
+                />
+              </div>
+            </div>
+            <Button onClick={() => setModalState(true)} className="">Volunteer Now</Button>
+          </div>
+        </div>
+        <div
+          className={`fixed top-0 w-[100%] h-[100%] bg-gray-900 bg-opacity-90 z-50 flex flex-col justify-center items-center ${
+            modalState == false ? "hidden" : "block"
+          }`}
+        >
+          <div className="w-[100%] text-right p-3">
+          <Button onClick={() => setModalState(false)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Button>
+          </div>
+          <VolunteerModal {...{modalState, setModalState}} />
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
---
+++ src/app/components/VolunteerModal.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import Link from "next/link";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Please enter your name",
+  }),
+  age: z
+    .number({
+      message: "Please enter your age",
+    })
+    .min(18, {
+      message: "You must be at least 18 to volunteer with us",
+    }),
+  email: z.string().email({
+    message: "Please enter your email address",
+  }),
+  statement: z.string().min(100, {
+    message: "Must be at least 100 characters",
+  }),
+});
+
+export default function VolunteerModal({
+  setModalState,
+}: {
+  setModalState: Function;
+}) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      age: 18,
+      email: "",
+      statement: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    setModalState(false);
+  }
+
+  return (
+    <div className="w-[80%] h-[80%] m-auto md:w-[60%]">
+      <ScrollArea className="h-[100%] w-[100%] bg-white rounded-sm border p-4">
+        <h1 className="text-xl font-bold mb-4">
+          Fill in the form below and we'll be in touch with you soon!
+        </h1>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Full name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="age"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Age</FormLabel>
+                  <FormControl>
+                    <Input type="number" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Your email</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="statement"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Why do you want to volunteer with us?</FormLabel>
+                  <FormDescription>
+                    In 100 words or more, tell us why you want to volunteer with
+                    NGO name
+                  </FormDescription>
+                  <FormControl>
+                    <Textarea placeholder="Your statement" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </ScrollArea>
+    </div>
+  );
+}
